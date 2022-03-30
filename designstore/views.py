3ba969fb9c73bb723d754c3c5a5cc12b4bc69685
djmolloy57 <dj_molloy57@hotmail.com>
2022-03-30 05:28:08 +0000

    added csrf_token to form_page template and in views outputed form field printng to commandline

--- designstore/views.py
+++ designstore/views.py
@@ -17,7 +17,14 @@ def viewitem(request):
 
 def form_name_view(request):
     form = forms.FormName()
-    #if request.method == 'POST':
-    #    form = forms.FormName(request.POST)
-    context = {'form': form}
+    if request.method == 'POST':
+        form = forms.FormName(request.POST)
+
+        if form.is_valid():
+        
+            print("VALIDATION SUCCESS!")
+            print("NAME: " +form.cleaned_data['name'])
+            print("EMAIL: "+form.cleaned_data['email'])
+            print("TEXT: "+form.cleaned_data['text'])
+
     return render(request,'designstore/form_page.html', {'form': form})
